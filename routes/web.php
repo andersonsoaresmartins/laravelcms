b74a6b4950f18f60e36c36c981dd86cecfc70441
Anderson Soares Martins <asmartins@universal.ad>
2022-04-20 16:22:57 -0300

    INITIAL PREPARATION

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-//Route::get('/', 'Site\HomeController:class', 'index');
-
 Route::get('/', [App\Http\Controllers\Site\HomeController::class, 'index']);
 
 Route::prefix('dashboard')->group(function ()
 {
-    Route::get('/', [App\Http\Controllers\Admin\HomeController::class, 'index']);
+    Route::get('/', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin');
+    Route::get('login', [\App\Http\Controllers\Auth\LoginController::class, 'index'])->name('login');
 });
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
